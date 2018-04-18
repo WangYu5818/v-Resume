60fe074ee2305b91e1c569793d68ffb025970ad1
pagenini <515598423@qq.com>
2018-04-18 22:07:56 +0800

    resume

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container" id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+.container{
+  width:100%;
+  max-width:1280px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/HeaderImg.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="headerImg">
+	  <img :src="pictureUrl" alt="点击选择头像" />
+	  <input type="file" class="file-button" @change="onFileChange" />
+	</div>
+</template>
+<style></style>
+<script>
+	export default{
+		data(){
+	      return {
+	        images:[],
+	        pictureUrl:'../../static/img/headerImg.jpg'
+	      }
+	 	},
+		methods:{
+		    test () {
+		    console.log(this.message)
+		},
+	    onFileChange (e) {
+	      var files = e.target.files || e.dataTransfer.files
+	      if (!files.length) return
+	      this.createImage(files)
+	    },
+	    createImage (file) {
+	      var vm = this
+	      var reader = null
+	      reader = new window.FileReader()
+	      reader.readAsDataURL(file[0])
+	      reader.onload = function (e) {
+	        vm.pictureUrl = e.target.result
+	      }
+	    },
+	  },
+	}
+</script>
